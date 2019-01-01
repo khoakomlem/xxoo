aac90d166bfb1582fd14d1cb0af9f881e1fd8952
Khoa Dau <visualpascalc@gmail.com>
2019-01-01 05:47:37 -0800

    1

---
+++ client.js
@@ -0,0 +1,106 @@
+var package={
+	name:'',
+	mode:1,
+	text:'',
+	players:1,
+	time:''
+}
+var lastid='1 1';
+var timeout;
+function addTalk(data){
+	clearTimeout(timeout);
+	$('#talk').html('<p>'+data+'</p>');
+	timeout=setTimeout(function(){
+		$('#talk p').fadeOut();
+	},3000);
+}
+$('#create').click(function(){
+	$('#behind').css('filter','brightness(40%)');
+	$('#front').slideDown(500);
+});
+
+$('#close').click(function(){
+	$('#front').slideUp(10);
+	$('#behind').css('filter','brightness(100%)');
+})
+$('#error').click(function(){
+	alert('Vui lòng chụp ảnh màn hình lỗi và gửi cho tui mau !! >:3');
+	window.open('http://www.facebook.com/nghienpascal', '_blank');
+});
+$('#ok').click(function(){
+	if ($('#name').val()=='' || $('#text').val()==''){
+		return 0;
+	}
+	var today = new Date();
+	var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+	var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+	var dateTime = date+' '+time;
+	package={
+		name:$('#name').val(),
+		mode:'1 v 1',
+		text:$('#text').val(),
+		players:1,
+		time:dateTime
+	}
+	room=socket.id;
+	socket.emit('create',package);
+	$('#front').slideUp(10);
+	$('#behind').css('filter','brightness(100%)');
+	$('#man1').fadeOut();
+	$('#man2').fadeIn();
+	$('#talk').html('<p>WAITING OTHER PLAYERS . . .</p>');
+});
+var timeout;
+socket.on('message', data=>{
+	if (data=='chủ phòng will go first :P'){
+		alert('Ping :v');
+	}
+	addTalk(data);
+});
+socket.on('move', (data,ids)=>{
+	if (ngoai[room].players==1){
+		return 0;
+	}
+	var id=data.x+' '+data.y;
+	// alert(id);
+	if (ids!=socket.id){
+		lastid=id;
+		document.getElementById(id).style.backgroundColor='#99e579';
+	}
+	if (ids==socket.id){
+		if (lastid==''){
+			lastid=id;
+		}
+		document.getElementById(lastid).style.backgroundColor='#d8d8d8';
+		// document.getElementById(id).style.backgroundColor='#62c162';
+	}
+	document.getElementById(id).innerHTML='<p>'+data.text+'</p>';
+})
+socket.on('exit', (reason)=>{
+	if (reason=='end')
+		addTalk('Trận đã kết thúc!!');
+	if (reason=='dis')
+		alert('May be someone disconnected then the game ended :(');
+	setTimeout(function(){
+		for (i=1; i<=20; i++){
+			for (j=1; j<=40; j++){
+				var t=j+' '+i;
+				document.getElementById(t).innerHTML='';
+				document.getElementById(t).style.backgroundColor='#d8d8d8';
+			}
+		}
+		$('#front').slideUp(10);
+		$('#behind').css('filter','brightness(100%)');
+		$('#man1').fadeIn();
+		$('#man2').fadeOut();
+	},2000);
+	
+
+})
+
+socket.on('ketqua', (data)=>{
+	if (data='player1')
+		alert('Chủ phòng thắng :D');
+	else
+		alert('Chủ phòng thua :D');
+})
---
+++ server.js
@@ -0,0 +1,289 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+var path = require('path');
+app.get('/', function(req, res){
+	var express=require('express');
+	app.use(express.static(path.join(__dirname)));
+	res.sendFile(path.join(__dirname, '../pacman', 'index.html'));
+});
+var board={};
+var idserver=[];
+var text=[];
+var danh=[];
+function make2DArray(col, row, vf){
+	var arr=new Array(col);
+	for (var m=0; m<col; m++){
+		arr[m]=new Array(row);
+		arr[m].fill(vf);
+	}
+	return arr;
+}
+var check={};
+var room={};
+io.on('connection', function(socket){
+	socket.on('create', data=>{
+		var t=socket.id;
+		check[t]=make2DArray(45,45,'none');
+		board[socket.id] = {
+			name:data.name,
+			mode:data.mode,
+			text:data.text,
+			players:data.players,
+			time:data.time,
+			room:socket.id
+		} 
+		room[socket.id]={
+			player1:socket.id,
+			player2:''
+		}
+		danh[socket.id]=true;
+		socket.join(socket.id);
+		idserver.push(socket.id);
+		io.emit('board',board,idserver);
+	});
+	socket.on('disconnect', ()=>{
+		
+		console.log(j);
+		for (var i in room){
+			if (room[i].player1==socket.id || room[i].player2==socket.id){
+				io.sockets.in(room[i].player1).emit('exit','dis');
+				var j=-1;
+				for (var k in idserver){
+					j++;
+					if (idserver[k]==room[i].player1){
+						idserver.splice(j,1);
+						delete board[idserver[k]];
+						break;
+					}
+				}
+				break;
+			}
+		};
+	});
+
+	socket.on('move', (x,y,id)=>{
+		
+		try{
+		check[id][y][x]=text[socket.id];
+		if (danh[socket.id]==false){
+			return 0;
+		}
+		if (danh[room[id].player1])
+			danh[room[id].player1]=false;
+		else
+			danh[room[id].player1]=true;
+
+		if (danh[room[id].player2])
+			danh[room[id].player2]=false;
+		else
+			danh[room[id].player2]=true;
+		}
+		catch(err){
+			console.log(err);
+		}
+		var move={
+			x:x,
+			y:y,
+			text:text[socket.id]
+		}
+		// console.log(text);
+		var chainx=0;
+		var chaino=0;
+		io.sockets.in(id).emit('move', move, socket.id);
+		//ngang
+		for (y=1; y<=20; y++)
+			for (x=1; x<=40; x++){
+				if (check[id][y][x]=='X') {
+					chainx++;
+					chaino=0;
+				}
+				if (check[id][y][x]=='O') {
+					chaino++;
+					chainx=0;
+				}
+				if (check[id][y][x]=='none'){
+					chainx=0;
+					chaino=0;
+				}
+				if (chainx>=5)
+					io.sockets.in(id).emit('ketqua','player1');
+				if (chaino>=5)
+					io.sockets.in(id).emit('ketqua','player2');
+				if (chainx>=5 || chaino>=5){
+					io.sockets.in(id).emit('exit','end');
+					var j=-1;
+					for (var k in idserver){
+						j++;
+						if (idserver[k]==id){
+							idserver.splice(j,1);
+							delete board[idserver[k]];
+							break;
+						}
+					}
+					chainx=0;
+					chaino=0;
+					break;
+				}
+			}
+
+		//doc
+		chainx=0;
+		chaino=0;
+		for (x=1; x<=40; x++)
+			for (y=1; y<=20; y++){
+				if (check[id][y][x]=='X') {
+					chainx++;
+					chaino=0;
+				}
+				if (check[id][y][x]=='O') {
+					chaino++;
+					chainx=0;
+				}
+				if (check[id][y][x]=='none'){
+					chainx=0;
+					chaino=0;
+				}
+				if (chainx>=5)
+					io.sockets.in(id).emit('ketqua','player1');
+				if (chaino>=5)
+					io.sockets.in(id).emit('ketqua','player2');
+				if (chainx>=5 || chaino>=5){
+					io.sockets.in(id).emit('exit','end');
+					var j=-1;
+					for (var k in idserver){
+						j++;
+						if (idserver[k]==id){
+							idserver.splice(j,1);
+							delete board[idserver[k]];
+							break;
+						}
+					}
+					chainx=0;
+					chaino=0;
+					break;
+				}
+			}
+		//cheo trái sang phải
+		chainx=0;
+		chaino=0;
+		var p=0;var tmp=0;
+		var x=0; var y=0;
+		while (x!=40 || y!=20){
+			p++;
+			if (p>40) {p=40;tmp++};
+			x=p;
+			y=tmp;
+			for (x=p; x>=1; x--){
+				y++;
+				if (y>20) {y=20;break;}
+				if (check[id][y][x]=='X') {
+					chainx++;
+					chaino=0;
+				}
+				if (check[id][y][x]=='O') {
+					chaino++;
+					chainx=0;
+				}
+				if (check[id][y][x]=='none'){
+					chainx=0;
+					chaino=0;
+				}
+				if (chainx>=5)
+					io.sockets.in(id).emit('ketqua','player1');
+				if (chaino>=5)
+					io.sockets.in(id).emit('ketqua','player2');
+				if (chainx>=5 || chaino>=5){
+					io.sockets.in(id).emit('exit','end');
+					var j=-1;
+					for (var k in idserver){
+						j++;
+						if (idserver[k]==id){
+							idserver.splice(j,1);
+							delete board[idserver[k]];
+							break;
+						}
+					}
+					chainx=0;
+					chaino=0;
+					break;
+				}
+			}
+		
+		}
+		chainx=0;
+		chaino=0;
+		var p=21;var tmp=0;
+		var x=1; var y=0;
+		while (x!=40 || y!=1){
+			p--;
+			if (p<1) {p=1;tmp++};
+			x=tmp;
+			for (y=p; y<=20; y++){
+				x++;
+				if (x>40) {x=40;break;}
+				if (check[id][y][x]=='X') {
+					chainx++;
+					chaino=0;
+				}
+				if (check[id][y][x]=='O') {
+					chaino++;
+					chainx=0;
+				}
+				if (check[id][y][x]=='none'){
+					chainx=0;
+					chaino=0;
+				}
+				if (chainx>=5)
+					io.sockets.in(id).emit('ketqua','player1');
+				if (chaino>=5)
+					io.sockets.in(id).emit('ketqua','player2');
+				if (chainx>=5 || chaino>=5){
+					io.sockets.in(id).emit('exit','end');
+					var j=-1;
+					for (var k in idserver){
+						j++;
+						if (idserver[k]==id){
+							idserver.splice(j,1);
+							delete board[idserver[k]];
+							break;
+						}
+					}
+					chainx=0;
+					chaino=0;
+					break;
+				}
+				
+			}
+		}
+
+	});
+
+	socket.on('join', id=>{
+		
+		if (room[id].player2==''){
+			socket.join(id);
+			board[id].players=2;
+			io.sockets.in(id).emit('message', socket.id+'has joined the game!');
+			io.sockets.in(id).emit('message', 'chủ phòng will go first!');
+			io.sockets.in(socket.id).emit('join success', id);
+			room[id].player2=socket.id;
+			text[room[id].player1]='X';
+			danh[room[id].player1]=true;
+			danh[socket.id]=false;
+			text[socket.id]='O';
+		} else {
+			socket.emit('join failed', id);
+		}
+		
+		
+	})	
+
+});
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
+setInterval(function(){
+	io.emit('board',board,idserver);
+	// console.log(id);
+},1000);
