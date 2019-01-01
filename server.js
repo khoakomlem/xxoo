1a1887719f19f4d96484d4772903952cce94c8b1
Khoa Dau <visualpascalc@gmail.com>
2019-01-01 05:56:20 -0800

    3

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require('path');
 app.get('/', function(req, res){
 	var express=require('express');
 	app.use(express.static(path.join(__dirname)));
-	res.sendFile(path.join(__dirname, '../pacman', 'index.html'));
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 var board={};
 var idserver=[];
